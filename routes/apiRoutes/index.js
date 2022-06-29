a8b3eb4f2aa17e163666c0f56b3ec29b4e0df704
Lake Bailey <lake.bailey@icloud.com>
2022-06-28 22:31:48 -0400

    renamed db.json to notes.json, commented application

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,13 +2,15 @@ const path = require('path');
 const router = require('express').Router();
 const { addNewNote, deleteById } = require('../../lib/notes');
 
-const { notes } = require('../../db/db');
+const { notes } = require('../../db/notes');
 
+// Handles GET request for existing notes.
 router.get('/notes', (req, res) => {
     results = notes;
     res.json(results);
 })
 
+// Adds a new note using the request body provided by client side javascript's handling of user input
 router.post('/notes', (req, res) => {
     let input = req.body
     const newNote = {
@@ -16,10 +18,11 @@ router.post('/notes', (req, res) => {
         title: input.title,
         text: input.text
     }
-    addNewNote(newNote, notes);
-    res.sendStatus(200);
+    let note = addNewNote(newNote, notes);
+    res.json(note);
 })
 
+// Deletes a note using the unique id provided in the request parameters
 router.delete('/notes/:id', (req, res) => {
     const id = req.params.id;
     deleteById(id, notes);
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
 
+// Handles GET request for the index page
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'))
 })
 
+
+// Handles GET request for the notes page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'))
 })
